d63f99bfa81b65b385e9ac60b9e067dc28afd3cf
nitunwa <nitun.2@gmail.com>
2020-06-09 23:46:00 -0700

    udate review table

--- TravelApi/Migrations/20200610064019_Initial.cs
+++ TravelApi/Migrations/20200610064019_Initial.cs
@@ -13,8 +13,8 @@ namespace TravelApi.Migrations
                 {
                     PlaceId = table.Column<int>(nullable: false)
                         .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
-                    City = table.Column<string>(nullable: true),
-                    Country = table.Column<string>(nullable: true),
+                    City = table.Column<string>(maxLength: 255, nullable: false),
+                    Country = table.Column<string>(maxLength: 255, nullable: false),
                     Rating = table.Column<double>(nullable: false)
                 },
                 constraints: table =>
@@ -23,14 +23,32 @@ namespace TravelApi.Migrations
                 });
 
             migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Username = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Token = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
                 name: "Reviews",
                 columns: table => new
                 {
                     ReviewId = table.Column<int>(nullable: false)
                         .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
-                    ReviewText = table.Column<string>(nullable: true),
+                    ReviewText = table.Column<string>(maxLength: 255, nullable: false),
                     Rating = table.Column<double>(nullable: false),
-                    PlaceId = table.Column<int>(nullable: false)
+                    PlaceId = table.Column<int>(nullable: true),
+                    UserId = table.Column<int>(nullable: true)
                 },
                 constraints: table =>
                 {
@@ -40,13 +58,24 @@ namespace TravelApi.Migrations
                         column: x => x.PlaceId,
                         principalTable: "Places",
                         principalColumn: "PlaceId",
-                        onDelete: ReferentialAction.Cascade);
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
                 });
 
             migrationBuilder.CreateIndex(
                 name: "IX_Reviews_PlaceId",
                 table: "Reviews",
                 column: "PlaceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_UserId",
+                table: "Reviews",
+                column: "UserId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -56,6 +85,9 @@ namespace TravelApi.Migrations
 
             migrationBuilder.DropTable(
                 name: "Places");
+
+            migrationBuilder.DropTable(
+                name: "Users");
         }
     }
 }
